83ca3dee363f29a8ead24673c6d41c72b007b6fe
Noah Hughes <hughesna@wssu.edu>
2025-12-04 15:16:07 +0000

    Worked on merger stuff

--- waveform_analysis.py
+++ waveform_analysis.py
@@ -40,40 +40,79 @@ ldata = TimeSeries.fetch_open_data('L1', *segment, verbose = True, cache = True)
 print("Livingston Strain")
 print(ldata)
 print("")
+print(ldata.dt)
+print("")
+
+print(ldata.value)
+
+from pycbc.waveform.waveform import get_td_waveform
+apx = 'IMRPhenomD'
+hp1, _ = get_td_waveform(approximant=apx,
+                         mass1=137,
+                         mass2=101,
+                         delta_t=float(ldata.dt),
+                         f_lower=2)
+
+print("")
+print(ldata.times)
+print("")
+print(hp1.sample_times)
+'''
+plt.figure()
+plt.title("Signal in the data")
+plt.plot(ldata.times, ldata.value)
+plt.plot(hp1.sample_times, 10 * hp1)
+plt.xlabel('Time (s)')
+plt.ylabel('Normalized amplitude')
+plt.savefig('waveform-overlay.png')
+print("Overlay plot done!")
+'''
+
 
 # specify the sample rate.
 # LIGO raw data is sampled at 16384 Hz (=2^14 samples/second).
 # It captures signal frequency content up to f_Nyquist = 8192 Hz.
 # Here, we will make the computation faster by sampling at a lower rate.
-sample_rate = 4096 # samples per second
-data_length = len(ldata) # seconds
-data = ldata
+# sample_rate = 4096 # samples per second
+# data_length = len(ldata) # seconds
+# data = ldata
 
 # ldata.plot()
 # plt.savefig('ligo-strain-test.png')
 # print("plot done!")
 
 # plt.plot(times, data)
-data.plot()
-plt.xlabel('Time(s)')
-plt.savefig('waveform-rawdata.png')
-print("Strain data plot done!")
+# data.plot()
+# plt.xlabel('Time(s)')
+# plt.savefig('waveform-rawdata.png')
+# print("Strain data plot done!")
+
+
 
 # data = numpy.random.normal(size=[sample_rate * data_length])
-times = numpy.arange(len(data)) / float(sample_rate)
+# times = numpy.arange(len(data)) / float(sample_rate)
 
-from pycbc.waveform.waveform import get_td_waveform
+# from pycbc.waveform.waveform import get_td_waveform
+# plt.figure()
+# plt.title("The waveform hp1")
+# plt.plot(hp1.sample_times, hp1)
+# plt.xlabel('Time (s)')
+# plt.ylabel('Normalized amplitude')
+# plt.savefig('waveform_approx.png')
+# print("Waveform plot done!")
+
+
 
 # the "approximant" (jargon for parameterized waveform family).
 # IMRPhenomD(a phenomenological Inspiral–Merger–Ringdown wafeform model) is defined in the frequency domain, but we'll get it in the time domain (td).
 # It runs fast, but it doesn't include effects such as non-aligned component spin, or higher order modes.
-apx = 'IMRPhenomD'
+# apx = 'IMRPhenomD'
 
-hp1, _ = get_td_waveform(approximant=apx,
-                         mass1=137,
-                         mass2=101,
-                          delta_t=1.0/sample_rate,
-                         f_lower=2)
+# hp1, _ = get_td_waveform(approximant=apx,
+#                          mass1=137,
+#                          mass2=101,
+#                           delta_t=1.0/sample_rate,
+#                          f_lower=2)
 
 # hp1 = hp1 / max(numpy.correlate(hp1, hp1, mode='full'))**0.5
 
@@ -81,23 +120,16 @@ hp1, _ = get_td_waveform(approximant=apx,
 # The duration (for frequency above f_lower=25 Hz) is only 3 or 4 seconds long.
 # The waveform is "tapered": slowly ramped up from zero to full strength, over the first second or so.
 # It is zero-padded at earlier times.
-plt.figure()
-plt.title("The waveform hp1")
-plt.plot(hp1.sample_times, hp1)
-plt.xlabel('Time (s)')
-plt.ylabel('Normalized amplitude')
-plt.savefig('waveform_approx.png')
-print("Waveform plot done!")
+# plt.figure()
+# plt.title("The waveform hp1")
+# plt.plot(hp1.sample_times, hp1)
+# plt.xlabel('Time (s)')
+# plt.ylabel('Normalized amplitude')
+# plt.savefig('waveform_approx.png')
+# print("Waveform plot done!")
 
 
-plt.figure()
-plt.title("Signal in the data")
-plt.plot(times, data)
-plt.plot(hp1.sample_times, 10 * hp1)
-plt.xlabel('Time (s)')
-plt.ylabel('Normalized amplitude')
-plt.savefig('waveform-overlay.png')
-print("Overlay plot done!")
+
 
 ## Cross Correlation is currently taking a very long time to complete.
 '''
