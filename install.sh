9cbf648e62a9662c68d669eb5981e5b1830698ff
nahughes <hughesna@wssu.edu>
2025-12-08 12:56:09 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,10 @@
 pip -q install 'gwosc==0.7.1'
 pip -q install 'gwosc==0.7.1'
 pip -q install 'gwpy==3.0.12'
-pip install -q PyCBC==2.4.1 lalsuite==7.25
+pip -q install 'PyCBC==2.6.0' 
+pip -q install 'lalsuite==7.25'
+pip -q install 'bilby==2.6.0'
+pip -q install 'dynesty==2.1.5'
+pip -q install 'corner==2.2.3'
+pip -q install 'astropy==7.0.1'
+pip -q install 'gwpopulation==0.10.0'
